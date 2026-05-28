291f815af403e83f36cfed8c5c0fd14dc6b46ed6
rusty1s <matthias.fey@tu-dortmund.de>
2026-05-28 13:35:28 +0000

    clipboard

--- lua/happy-hacking/options.lua
+++ lua/happy-hacking/options.lua
@@ -2,7 +2,21 @@ vim.g.mapleader = " "
 vim.g.maplocalleader = " "
 
 local opt = vim.opt
-opt.clipboard = "unnamedplus"
+
+if vim.env.SSH_TTY or vim.env.SSH_CONNECTION then
+  local osc52_copy = require("vim.ui.clipboard.osc52").copy("+")
+  vim.api.nvim_create_autocmd("TextYankPost", {
+    desc = "Copy yanks to the local clipboard over SSH",
+    callback = function()
+      if vim.v.event.operator == "y" and vim.v.event.regname ~= "_" then
+        osc52_copy(vim.v.event.regcontents)
+      end
+    end,
+  })
+else
+  opt.clipboard = "unnamedplus"
+end
+
 opt.number = true
 opt.relativenumber = true
 opt.numberwidth = 4
